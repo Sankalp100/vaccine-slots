86d8cb942140b331bea3427f1bfb4a127002b8c0
Sankalp Gour <sankalpgour06@gmail.com>
2021-05-16 12:48:53 +0530

    vaccine-slots

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: true
+    };
+  }
+  async getUsersData() {
+    const res = await axios.get(
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=312&date=16-05-2021"
+    );
+    console.log(res.data);
+    this.setState({ loading: false, users: res.data.centers });
+  }
+
+  // useEffect() {
+  //   this.getUsersData();
+  //   const interval = setInterval(() => {
+  //     this.getUsersData();
+  //   }, 10000);
+  //   return () => clearInterval(interval);
+  // }
+
+  componentDidMount() {
+    this.getUsersData();
+    // this.useEffect();
+    const interval = setInterval(() => {
+      this.getUsersData();
+    }, 10000);
+    return () => clearInterval(interval);
+  }
+
+  render() {
+    const columns = [
+      {
+        Header: "CENTER_ID",
+        accessor: "center_id"
+      },
+      {
+        Header: "NAME",
+        accessor: "name"
+      },
+
+      {
+        Header: "PIN",
+        accessor: "pincode"
+      },
+      {
+        Header: "VACCINE",
+        accessor: "sessions.0.vaccine"
+      },
+      {
+        Header: "AGE",
+        accessor: "sessions.0.min_age_limit"
+      },
+      {
+        Header: "DOSE1",
+        accessor: "sessions.0.available_capacity_dose1"
+      },
+      {
+        Header: "DOSE2",
+        accessor: "sessions.0.available_capacity_dose2"
+      }
+    ];
+    return <ReactTable data={this.state.users} columns={columns} />;
+  }
 }
-
-export default App;
